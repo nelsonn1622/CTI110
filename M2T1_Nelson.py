7bf72fb5691ae0de88ab0119a1eab7fbd0078ff4
nelsonn1622 <31700792+nelsonn1622@users.noreply.github.com>
2017-09-06 11:17:21 -0400

    Add files via upload

---
+++ M2T1_Nelson.py
@@ -0,0 +1,38 @@
+# CTI 110
+# M1T2- sales Prediction
+# 8/30/2017
+# NelsonN
+
+
+# Suggested varables: totalSales, annualprofit
+
+# ask the user for total sales
+usertotalSales = int(input("what are the total Sales: "))
+
+
+#calculate the profit amount
+annualprofit = 0.23 * usertotalSales
+
+#print the profit amount
+print("the annual profit is: ", annualprofit)
+
+#
+
+
+
+'''
+# program to add two numbers
+# userTyped = ("Tpe a number: ")
+#userNumber = int(userTyped)
+
+# get the numbers
+firstNumber = int(input("Typed a number: "))
+secondNumber = int(input("Type another number: "))
+
+
+#  add them
+answer = firstNumber + secondNumber
+
+#print the results
+print ("The answer is", answer)
+'''
